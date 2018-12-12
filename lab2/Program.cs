5a49e84148c626b8738494d3a139a11344544245
mag1987 <mag987@list.ru>
2018-12-12 21:57:06 +0700

    добавлен абстрактный класс и виртуальные методы, два конструктора

--- lab2/Program.cs
+++ lab2/Program.cs
@@ -20,6 +20,10 @@ namespace lab2
         int WorkerID { get; }
         int TermOfEmployment { get; set; }
     }
+    public interface IGeneratorID
+    {
+        int GenerateID(object Object);
+    }
     public interface IContainer<IContainment>
     {
          int AvailableSpace { get; set; }
@@ -31,9 +35,21 @@ namespace lab2
         public int WorkerID { get; }
         public int TermOfEmployment { get; set; }
         public string ProfessionName { get; set; }
+        public double BaseSalary { get; set; }
+        public List<Aviary> AviaryResponsibilities = new List<Aviary>();
         public double Salary()
         {
-
+            return BaseSalary * TermOfEmployment * AviaryResponsibilities.Count;
+        }
+       
+        Employee(int workerID): this (workerID, "none")
+        {
+        }
+        Employee(int workerID, string professionName)
+        {
+            WorkerID = workerID;
+            ProfessionName = professionName;
+            BaseSalary = 0;
         }
 
     }
@@ -61,26 +77,39 @@ namespace lab2
     { 
         
     }
-    public class Aviary : Container, IContainment
+    public  abstract class Aviary : Container, IContainment
     {
        public int RequiredSpace { get; set; }
+        public int Temperature { get; set; }
+        public virtual int HeatPumpPower()
+        {
+            return RequiredSpace * Temperature;
+        }
     }
     public class Aquarium : Aviary
     {
-        public int Temperature { get; set; }
         public int Salinity { get; set; }
+        public override int HeatPumpPower()
+        {
+            return RequiredSpace * Temperature * Salinity;
+        }
     }
     public class Serpentarium:Aviary
     {
-        public int Temperature { get; set; }
+       
     }
     public class BirdCage : Aviary
     {
-        public int Heihgt { get; set; }
+        public int Height { get; set; }
+        public override int HeatPumpPower()
+        {
+            return (RequiredSpace+Height) * Temperature;
+        }
     }
     class Animal:IContainment
     {
        public int RequiredSpace { get; set; }
+        public List<string> Meals = new List<string>(); 
     }
    
     class Program
@@ -88,7 +117,7 @@ namespace lab2
         static void Main(string[] args)
         {
             Zoo z = new Zoo();
-            Aviary aviary = new Aviary();
+            Aquarium aviary = new Aquarium();
             Animal animal = new Animal();
             z.Add(aviary);
             aviary.Add(animal);
